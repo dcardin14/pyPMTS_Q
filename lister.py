4861a86754f2af29fe6c067ba7eb84749329e780
Daniel <dcardin14@gmail.com>
2022-12-26 09:52:49 -0600

    now it will omit the ARCHIVE directory

--- lister.py
+++ lister.py
@@ -8,13 +8,16 @@ import csv
 root_directory = 'O:\\FINANCE\\SS-CA\\Contracting\\PaymentRequests'
 
 # Open the output CSV file for writing
-with open('directory_listingr.csv', 'w', newline='') as csvfile:
+with open('directory_listing.csv', 'w', newline='') as csvfile:
   # Create a CSV writer object
   writer = csv.writer(csvfile)
   # Write the column headers
   writer.writerow(['Name', 'Size', 'Modified Time', 'Path'])
   # Iterate over the directories and files in the root directory
   for root, dirs, files in os.walk(root_directory):
+    # Skip the ARCHIVE subdirectory
+    if 'ARCHIVE' in dirs:
+      dirs.remove('ARCHIVE')
     # Iterate over the files in the current directory
     for filename in files:
       # Get the file's path
@@ -22,11 +25,6 @@ with open('directory_listingr.csv', 'w', newline='') as csvfile:
       # Get the file's size and modified time
       file_size = os.stat(file_path).st_size
       file_modified_time = os.stat(file_path).st_mtime
-   
       # Write the file's information to the CSV file
       writer.writerow([filename, file_size, file_modified_time, file_path])
-
-
-
-
 
